1ef9b8c1dfaaf35039872ff1f1306ef7668eaa96
Eva.Wu <shuting.wu@qishon.com>
2017-04-22 19:23:40 +0800

    modify property file

--- src/main/scripts/install/install.sh
+++ src/main/scripts/install/install.sh
@@ -1,26 +1,28 @@
 #!/bin/sh
 groupadd es
 useradd -g es es
+ES_HOME=/server/es
 
 mkdir -p /server
-unzip es.zip -d /server
-rm /server/es/lib/esservice-*.jar -rf
-
+unzip -o es.zip -d /server
+rm $ES_HOME/lib/es-*.jar -rf
+version=v1.0.0
 
 mkdir -p /logs/es
-ES_HOME=/server/es
-dos2unix $ES_HOME/bin/*
+
+dos2unix $ES_HOME/bin/*.properties
 
 #配置搜素引擎IP
 echo -e "\n 请输入搜索引擎IP： \c"
 read ES_HOST
-sed -i "s;^elasticsearch.host=.*;elasticsearch.host=$ES_HOST;g" $ES_HOME/bin/elasticsearch.properties
+sed -i "s;^elasticsearch.host=.*;elasticsearch.host=$ES_HOST;g" $ES_HOME/bin/elasticsearch.properties 
 
 chown es:es /server/es -R
 chown es:es /logs/es -R
 
 #设置开机启动服务
 cp es /etc/init.d/
+chown es:es /etc/init.d/es
 chmod 751 /etc/init.d/es
 chkconfig --add es
 chkconfig --level 35 es on
@@ -33,7 +35,8 @@ if [ -z "$(grep "9100" /etc/sysconfig/iptables)" ] ; then
 	service iptables save
 	service iptables restart
 fi
-
+#记录版本
+echo -e `date "+%c"` install $version >> $ES_HOME/version
 # java -verbose -classpath .:/server/elastic/lib/*:/usr/local/jdk/lib/*.jar ElasticServer
 #启动服务
 service es restart
